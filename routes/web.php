6f35f911f6f7401467a6321ddfefcda09be1090b
jmespitiag <jmespitiag@eafit.edu.co>
2024-02-21 08:32:02 -0500

    Fixed

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,19 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
+
+
 |
 */
+
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
+
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
+
+Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save"); 
+
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show"); 
+
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index"); 
 
 Route::get('/about', function () {
@@ -24,4 +35,5 @@ Route::get('/about', function () {
     ->with("description", $description)
     ->with("author", $author);
    })->name("home.about");
+
 
